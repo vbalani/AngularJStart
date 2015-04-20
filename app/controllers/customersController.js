1b5ba6d31517431efd4d2460d1aaa1927ef008d2
Vikas Balani <vbalani@usa.net>
2015-04-20 19:35:08 -0400

    added factory concept

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -20,7 +20,7 @@
         $scope.sortBy = 'name';
         $scope.reverse = false;
         
-        $scope.customers= [{joined: '2000-12-02', name:'John', city:'Chandler', orderTotal: 9.9956}, {joined: '1965-01-25',name:'Zed', city:'Las Vegas', orderTotal: 19.99},{joined: '1944-06-15',name:'Tina', city:'New York', orderTotal:44.99}, {joined: '1995-03-28',name:'Dave', city:'Seattle', orderTotal:101.50}];
+        
         $scope.doSort = function(propName) {
            $scope.sortBy = propName;
            $scope.reverse = !$scope.reverse;
@@ -32,19 +32,29 @@
 //Option 3
 
 (function () {
-    
-    var CustomersController = function ($scope) {
+    // i can have any nbr of parameters below, depending on the factories and 
+    var CustomersController = function($scope, customersFactory) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
+        $scope.customers = [];
         
-        $scope.customers= [{id:1 ,joined: '2000-12-02', name:'John', city:'Chandler', orderTotal: 9.9956}, {id:2 ,joined: '1965-01-25',name:'Zed', city:'Las Vegas', orderTotal: 19.99},{id:3 ,joined: '1944-06-15',name:'Tina', city:'New York', orderTotal:44.99}, {id:4 ,joined: '1995-03-28',name:'Dave', city:'Seattle', orderTotal:101.50}];
+        function init () {
+            
+            $scope.customers = customersFactory.getCustomers();
+        };
+        
+        // init will fire only ones when this controller instantiates for the first time
+        init();
+        
         $scope.doSort = function(propName) {
            $scope.sortBy = propName;
            $scope.reverse = !$scope.reverse;
         };
     };
     
-    CustomersController.$inject = ['$scope'];
+    // minification avoidance boiler plate code, just add all the variables used in the controller function
+    // definition
+    CustomersController.$inject = ['$scope','customersFactory'];
     
     console.log('not the one with as');
     
